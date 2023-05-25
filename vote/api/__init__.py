1f1fc8b46fe080783a2ff2b5ea26d9ca653154ba
bogay <pojay11523@gmail.com>
2023-05-26 00:37:49 +0800

    feat(config): load from toml file

--- vote/api/__init__.py
+++ vote/api/__init__.py
@@ -2,12 +2,48 @@ from typing import Annotated
 from surrealdb import Surreal
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseSettings
+import toml
+
 from vote.domain.user import UserRepository, UserRepositoryImpl, UserService
 from vote.domain.topic import TopicService, TopicRepositoryImpl
 from vote.domain.vote import VoteService, VoteRepositoryImpl
 from vote.domain.auth import AuthConfig, AuthService
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='/auth/token')
+
+
+def toml_settings(settings: BaseSettings) -> dict:
+    return toml.load(open(settings.__config__.path))
+
+
+# https://docs.pydantic.dev/latest/usage/settings/#customise-settings-sources
+class VoteConfigToml(BaseSettings):
+    '''
+    Config for vote app loading from a toml file
+    '''
+    auth: AuthConfig
+
+    class Config:
+        path = 'vote.toml'
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            return (
+                init_settings,
+                toml_settings,
+                env_settings,
+                file_secret_settings,
+            )
+
+
+def get_vote_config():
+    return VoteConfigToml()
 
 
 async def get_db():
@@ -51,10 +87,8 @@ async def get_vote_service(db: Annotated[
     return VoteService(repo)
 
 
-def get_auth_service():
-    # TODO: load config
-    config = AuthConfig(
-        secret_key='foobar',
-        algorithm='HS256',
-    )
-    return AuthService(config)
+def get_auth_service(cfg: Annotated[
+    VoteConfigToml,
+    Depends(get_vote_config),
+]):
+    return AuthService(cfg.auth)
